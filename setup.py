65e4c72e6dba29c06763a8a928374facf5f4aa23
Yincmewy <91423824+Yincmewy@users.noreply.github.com>
2023-04-22 19:33:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="nonebot-plugin-runcode",
+  version="0.0.2",
+  author="Yincmewy",
+  author_email="2025173673@qq.com",
+  description="基于runoob.com的在线代码运行插件",
+  long_description=long_description,
+  install_requires=[
+          'nonebot', 'httpx', 'setuptools'
+      ],
+  long_description_content_type="text/markdown",
+  url="https://github.com/Yincmewy/nonebot_plugin_runcode",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
